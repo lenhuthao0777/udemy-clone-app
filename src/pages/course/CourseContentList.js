e961c44de37246aed1f28be7dc9cfe4a3d1d090d
Le Nhut Hao <lenhuthao0777@gmail.com>
2021-06-16 11:00:04 +0700

    update

--- src/pages/course/CourseContentList.js
+++ src/pages/course/CourseContentList.js
@@ -1,24 +1,37 @@
 import React, { useState } from "react";
-import { AiOutlineFile } from "react-icons/ai";
+import { AiOutlineFile, AiFillPlayCircle } from "react-icons/ai";
 import { FaAngleDown, FaAngleUp } from "react-icons/fa";
 
-function CourseContentList() {
+function CourseContentList({ item }) {
     const [subCourse, setSubCourse] = useState(false);
-    const courseList = [
-        {
-            id: 1,
-            title: "Course Orientation",
-            subCourse: [
-                {
-                    subId: 1,
-                    subTitle: "",
-                },
-            ],
-        },
-    ];
+    const ShowDrop = () => {
+        setSubCourse(!subCourse);
+    };
+
     return (
-        <div>
-            <h1>list course</h1>
+        <div className="course-content-list__item">
+            <div className="course-content-list__item-content" onClick={item.subCourse && ShowDrop}>
+                {subCourse == false ? <span>{item.iconOpen}</span> : <span>{item.iconClose}</span>}
+                <h3>{item.title}</h3>
+                <span>
+                    {item.hours} lectures • {item.minute} min
+                </span>
+            </div>
+            <div className="course-content-list__item-dropdown">
+                <ul>
+                    {subCourse &&
+                        item.subCourse.map((item) => {
+                            return (
+                                <li key={item.subId}>
+                                    <span>{item.icon}</span>
+                                    <h3>{item.subTitle}</h3>
+                                    <span>{item.textPreview}</span>
+                                    <span>{item.time}m</span>
+                                </li>
+                            );
+                        })}
+                </ul>
+            </div>
         </div>
     );
 }
